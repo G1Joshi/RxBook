ef1b5b6757cd6a010406fb59c3f0855fb87d3490
Jeevan Joshi <joshijeevan98@gmail.com>
2025-06-16 10:50:12 +0530

    add rx

--- PlaygroundDependencies/Package.swift
+++ PlaygroundDependencies/Package.swift
@@ -1,21 +1,19 @@
 // swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "PlaygroundDependencies",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PlaygroundDependencies",
-            targets: ["PlaygroundDependencies"]),
+            targets: ["PlaygroundDependencies"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PlaygroundDependencies"),
+            name: "PlaygroundDependencies"
+        ),
         .testTarget(
             name: "PlaygroundDependenciesTests",
             dependencies: ["PlaygroundDependencies"]
@@ -27,9 +25,11 @@ package.dependencies = [
     .package(url: "https://github.com/ReactiveX/RxSwift", from: "6.9.0")
 ]
 package.targets = [
-    .target(name: "PlaygroundDependencies",
+    .target(
+        name: "PlaygroundDependencies",
         dependencies: [
-            .product(name: "RxSwift", package: "RxSwift")
+            .product(name: "RxSwift", package: "RxSwift"),
+            .product(name: "RxCocoa", package: "RxSwift"),
         ]
     )
 ]
@@ -37,5 +37,5 @@ package.platforms = [
     .iOS("12.0"),
     .macOS("10.13"),
     .tvOS("12.0"),
-    .watchOS("4.0")
+    .watchOS("4.0"),
 ]
