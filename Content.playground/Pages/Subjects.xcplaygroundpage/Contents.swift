bef9cfd61e483ad06df6f9394feac43c078064ea
Jeevan Joshi <joshijeevan98@gmail.com>
2025-06-17 10:53:29 +0530

    add publish subject

---
+++ Content.playground/Pages/Subjects.xcplaygroundpage/Contents.swift
@@ -0,0 +1,41 @@
+import Foundation
+import PlaygroundSupport
+import RxCocoa
+import RxSwift
+import UIKit
+
+block("publishSubject") {
+    let subject: PublishSubject<String> = PublishSubject()
+
+    subject.onNext("A")
+
+    let subscription1 = subject.subscribe {
+        print("1", $0)
+    }
+
+    subject.onNext("B")
+
+    let subscription2 = subject.subscribe {
+        print("2", $0)
+    }
+
+    subject.onNext("C")
+
+    subscription1.dispose()
+
+    subject.onNext("D")
+
+    subject.onCompleted()
+
+    subject.onNext("E")
+
+    subscription2.dispose()
+
+    let subscription3 = subject.subscribe {
+        print("3", $0)
+    }
+
+    subject.onNext("F")
+
+    subscription3.dispose()
+}
